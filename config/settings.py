b83104a1bac7925c684f761c05deb588c5357b15
Abbos <161742244+Abbos20072005@users.noreply.github.com>
2024-06-19 21:37:13 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',systemctl daemon-reload
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
     'ckeditor'
