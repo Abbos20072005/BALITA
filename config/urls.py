1e0bb2158cca63554e748ef8e6240c1ce6e3551c
Abbos <161742244+Abbos20072005@users.noreply.github.com>
2024-06-26 21:05:01 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
-if settings.Debug:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
